89931a9b5ed9d7dacbaa6a5f9fb39b2d7c9a3a5d
fmcpetermux <70578351+fmcpetermux@users.noreply.github.com>
2021-09-12 20:35:58 +0700

    Update Iforgotthename.bat

--- Iforgotthename.bat
+++ Iforgotthename.bat
@@ -10,7 +10,7 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Reru
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, please paste new NGROK TOKEN in YML. Check Tunnel here: https://dashboard.ngrok.com/status/tunnels " && exit 
 echo User: administrator
-echo Pass: fmcpe1234
+echo Pass: fmcpe@1234
 curl -O  > out.txt 2>&1 https://raw.githubusercontent.com/fmcpetermux/Don-t-Banned-Me-I-will-save-my-project-here/main/no.ps1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './no.ps1'" > out.txt 2>&1
 diskperf -Y >nul
