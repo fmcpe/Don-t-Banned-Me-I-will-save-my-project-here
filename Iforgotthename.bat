028120a2a6cdd930cc0c8b94a538ab32cf203f28
fmcpetermux <70578351+fmcpetermux@users.noreply.github.com>
2021-09-12 18:45:26 +0700

    Update Iforgotthename.bat

--- Iforgotthename.bat
+++ Iforgotthename.bat
@@ -1,14 +1,15 @@
 @echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator fmcpe1234 /add >nul
+net user administrator fmcpe@1234 /add >nul
 net localgroup administrators administrator /add >nul
 echo To change another VM region, Create New organization (Your current VM location:  %LO% )
 echo Region Available: West Europe, Central US, East Asia, Brazil South, Canada Central, Autralia East, UK South, South India
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Rerun failed jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, please paste new NGROK TOKEN in YML. Check Tunnel here: https://dashboard.ngrok.com/status/tunnels " && exit 
-echo User: Administrator
+echo User: administrator
 echo Pass: fmcpe1234
 curl -O  > out.txt 2>&1 https://raw.githubusercontent.com/fmcpetermux/Don-t-Banned-Me-I-will-save-my-project-here/main/no.ps1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './no.ps1'" > out.txt 2>&1
